510bfe56ac24885a26580581033f7feefa0271fe
kim-langer <kim.sophie.langer@hs-furtwangen.de>
2022-12-03 12:03:47 +0100

    teilaufgabe a fertig

--- aufgabe_8/keyboard.js
+++ aufgabe_8/keyboard.js
@@ -1,24 +1,59 @@
+function playsample(ton) {
+    ton.play();
+}
 let taste1 = document.querySelector("#taste1");
 taste1.addEventListener('click', function () {
     playsample(new Audio("a.mp3"));
 });
 let taste2 = document.querySelector("#taste2");
 taste2.addEventListener('click', function () {
-    playsample(new Audio("b.mp3"));
+    playsample(new Audio("af.mp3"));
 });
 let taste3 = document.querySelector("#taste3");
 taste3.addEventListener('click', function () {
-    playsample(new Audio("c.mp3"));
+    playsample(new Audio("b.mp3"));
 });
 let taste4 = document.querySelector("#taste4");
 taste4.addEventListener('click', function () {
+    playsample(new Audio("bf.mp3"));
+});
+let taste5 = document.querySelector("#taste5");
+taste5.addEventListener('click', function () {
+    playsample(new Audio("c.mp3"));
+});
+let taste6 = document.querySelector("#taste6");
+taste6.addEventListener('click', function () {
     playsample(new Audio("d.mp3"));
 });
-let taste5 = document.querySelector("#taste4");
-taste5.addEventListener('click', function () {
+let taste7 = document.querySelector("#taste7");
+taste7.addEventListener('click', function () {
+    playsample(new Audio("df.mp3"));
+});
+let taste8 = document.querySelector("#taste8");
+taste8.addEventListener('click', function () {
     playsample(new Audio("e.mp3"));
 });
-function playsample(ton) {
-    ton.play();
-}
+let taste9 = document.querySelector("#taste9");
+taste9.addEventListener('click', function () {
+    playsample(new Audio("ef.mp3"));
+});
+let taste10 = document.querySelector("#taste10");
+taste10.addEventListener('click', function () {
+    playsample(new Audio("f.mp3"));
+});
+let taste11 = document.querySelector("#taste11");
+taste11.addEventListener('click', function () {
+    playsample(new Audio("g.mp3"));
+});
+let taste12 = document.querySelector("#taste12");
+taste12.addEventListener('click', function () {
+    playsample(new Audio("gf.mp3"));
+});
+let tonfolge = document.querySelector("#playallsounds");
+tonfolge.addEventListener('click', function () {
+    playsample(new Audio("gf.mp3"));
+});
+setInterval(function () {
+    tonfolge;
+}, 500);
 //# sourceMappingURL=keyboard.js.map
--- aufgabe_8/keyboard.ts
+++ aufgabe_8/keyboard.ts
@@ -1,4 +1,8 @@
 
+function playsample (ton: any)
+{ 
+    ton.play(); 
+}
 
 let taste1 = document.querySelector ("#taste1") as HTMLButtonElement
 taste1.addEventListener ('click', function (){
@@ -7,26 +11,64 @@ taste1.addEventListener ('click', function (){
 
 let taste2 = document.querySelector ("#taste2") as HTMLButtonElement
 taste2.addEventListener ('click', function (){
-    playsample (new Audio("b.mp3"))
+    playsample (new Audio("af.mp3"))
 })
 
 let taste3 = document.querySelector ("#taste3") as HTMLButtonElement
 taste3.addEventListener ('click', function (){
-    playsample (new Audio("c.mp3"))
+    playsample (new Audio("b.mp3"))
 })
 
 let taste4 = document.querySelector ("#taste4") as HTMLButtonElement
 taste4.addEventListener ('click', function (){
+    playsample (new Audio("bf.mp3"))
+})
+
+let taste5 = document.querySelector ("#taste5") as HTMLButtonElement
+taste5.addEventListener ('click', function (){
+    playsample (new Audio("c.mp3"))
+})
+
+let taste6 = document.querySelector ("#taste6") as HTMLButtonElement
+taste6.addEventListener ('click', function (){
     playsample (new Audio("d.mp3"))
 })
 
-let taste5 = document.querySelector ("#taste4") as HTMLButtonElement
-taste5.addEventListener ('click', function (){
+let taste7 = document.querySelector ("#taste7") as HTMLButtonElement
+taste7.addEventListener ('click', function (){
+    playsample (new Audio("df.mp3"))
+})
+
+let taste8 = document.querySelector ("#taste8") as HTMLButtonElement
+taste8.addEventListener ('click', function (){
     playsample (new Audio("e.mp3"))
 })
 
+let taste9 = document.querySelector ("#taste9") as HTMLButtonElement
+taste9.addEventListener ('click', function (){
+    playsample (new Audio("ef.mp3"))
+})
 
-function playsample (ton: any)
-{ 
-    ton.play(); 
-}
+let taste10 = document.querySelector ("#taste10") as HTMLButtonElement
+taste10.addEventListener ('click', function (){
+    playsample (new Audio("f.mp3"))
+})
+
+let taste11 = document.querySelector ("#taste11") as HTMLButtonElement
+taste11.addEventListener ('click', function (){
+    playsample (new Audio("g.mp3"))
+})
+
+let taste12 = document.querySelector ("#taste12") as HTMLButtonElement
+taste12.addEventListener ('click', function (){
+    playsample (new Audio("gf.mp3"))
+})
+
+let tonfolge = document.querySelector ("#playallsounds") as HTMLImageElement
+tonfolge.addEventListener ('click', function (){
+    playsample (new Audio("gf.mp3"))
+})
+
+setInterval(function() {
+    tonfolge
+ }, 500);
